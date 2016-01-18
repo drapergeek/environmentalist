9838df8a44438dd2db18a11953f5c720751575d9
Jason Draper <jason@drapergeek.com>
2016-01-18 14:43:55 -0500

    Basic setup for command line application

--- lib/enivronmentalist.ex
+++ lib/enivronmentalist.ex
@@ -1,2 +1,17 @@
 defmodule Enivronmentalist do
+  def main(args) do
+    args |> parse_args |> process
+  end
+
+  def process([]) do
+    IO.puts "No arguments given"
+  end
+
+  def process(options) do
+  end
+
+  defp parse_args(args) do
+    {options, _, _} = OptionParser.parse(args)
+    options
+  end
 end
