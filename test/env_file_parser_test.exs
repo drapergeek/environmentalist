e6785bb80acc31b4a0b6802254dbb35c56ba85ee
Jason Draper <jason@drapergeek.com>
2016-01-21 11:20:38 -0500

    Improve testing with ENV files
    Prior to this, we had a single env file in the repo itself that the tests were running against. Unfortunately this means that a single file needs to cover all our use cases. It also introduces a mystery guest into our tests.
    
    This gives us a set of functions that allow us to create a new ENV file with the given input. It will use a random file name so we don't step on our own toes.
    
    As we move forward, I'm guessing that we'll extract this logic into another module to be used for other tests but I think this is a good start.

--- test/env_file_parser_test.exs
+++ test/env_file_parser_test.exs
@@ -1,18 +1,41 @@
 defmodule EnvFileParserTest do
   use ExUnit.Case
 
+  setup do
+    File.rm_rf("tmp")
+    File.mkdir("tmp")
+    :ok
+  end
+
   test "excludes commments and empty lines" do
-    file_name = ".env.base"
-    parsed = EnvFileParser.for_file(file_name)
+    env_file = create_env_file ~s"""
+    SOMETHING=test
+
+    ANOTHER=test
+    NEW_ITEM=something_here
+    REQUIRED_ITEM=require-something
+    REMOTE_ITEM=remote-staging
+    #commented
+    """
+
+    parsed = EnvFileParser.for_file(env_file)
 
     assert Enum.count(parsed) == 5
   end
 
   test "creates a struct for each entry" do
-    file_name = ".env.base"
-    [first_entry | _ ] = EnvFileParser.for_file(file_name)
+    env_file = create_env_file ~s"""
+    MY_KEY=my_value
+    """
+    [first_entry | _ ] = EnvFileParser.for_file(env_file)
 
-    assert first_entry.key == "SOMETHING"
-    assert first_entry.value == "test"
+    assert first_entry.key == "MY_KEY"
+    assert first_entry.value == "my_value"
+  end
+
+  def create_env_file(text) do
+    file_name = "tmp/env-#{:random.uniform(10000)}"
+    File.write file_name, text, [:write]
+    file_name
   end
 end
