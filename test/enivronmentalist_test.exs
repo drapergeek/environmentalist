a6a2e0f8c6433a3f23f15673c444cd3e5247a8c6
Jason Draper <jason@drapergeek.com>
2016-01-21 17:52:41 -0500

    Basic test setup to see if it works

--- test/enivronmentalist_test.exs
+++ test/enivronmentalist_test.exs
@@ -1,7 +1,22 @@
 defmodule EnivronmentalistTest do
   use ExUnit.Case
   doctest Enivronmentalist
+  import EnvHelper
+
+  setup do
+    clear_files
+  end
 
   test "it adds missing entries" do
+    sample_file = create_env_file ~s"""
+    SOMETHING=test
+    """
+    actual_file = create_env_file ""
+
+    Enivronmentalist.process(sample_file: sample_file, actual_file: actual_file)
+
+    {:ok, file_text} = File.read actual_file
+
+    assert String.contains?(file_text, "SOMETHING=test")
   end
 end
